e457bc2a58b8d29696d707090394296674e42bbc
Nicholas Yang <nick@nicholasyang.com>
2014-10-21 19:08:36 -0400

    updated mage

--- Mage.java
+++ Mage.java
@@ -1,14 +1,24 @@
-public class Mage extends basechar {
-    public Mage(String n) {
-	setName(n);
-	setStrength(50);
-	setHealth(100);
-	setSpeed(75);
-	setManna(20);
+import java.util.Random;
+public class Mage extends basechar 
+{
+    Random randomNumber = new Random();
+    public Mage() 
+    {     
+	setStrength(40 + randomNumber.nextInt(20));
+	setHealth(90 + randomNumber.nextInt(20));
+	setSpeed(40 + randomNumber.nextInt(20));
+	setMana(90 + randomNumber.nextInt(20));
+
     }
-    public void attack(basechar other) {
-	other.setHealth(other.getHealth-manna);
-	System.out.println(this.getName()+" casts CRASH on "+other.getName()+" for "+manna+" damage!");
+    public void fireSpell(basechar other) 
+    {
+	this.setAttackStrength(this.getMana()/10 + randomNumber.nextInt(20)); 
+	System.out.println(this.getName()+" shoots FIRE at "+other.getName());
+	other.setDefending(true)
     }
+    public void heal (basechar other)
+    {
+	other.assignDamage(this)
+	this.increaseHealth(this.getMana()/9 + randomNumber.nextInt(20))
 }
 			   
