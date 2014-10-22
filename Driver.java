0332f8f3730c9ab655ea50d3f038c7119dda98fa
Derrick Lui <derricklui1@gmail.com>
2014-10-21 20:43:24 -0700

    Edited Driver to work with all 3 subclasses

--- Driver.java
+++ Driver.java
@@ -46,6 +46,172 @@ public class Driver {
 	    monster enemy = new monster();
 	    System.out.println();
 	    System.out.println("You are a "+character.toLowerCase()+" named "+newname.toUpperCase()+".");
+	    System.out.println("You walk through the forest with your mace on your shoulder.");
+	    try { Thread.sleep(1000); } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    try {
+		for (int i=0;i<parsing.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(parsing2[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    String warriorMethods[] = {"->BASH","->BLOCK"};
+	    System.out.println("You encountered an "+enemy.getName().toUpperCase()+"!");
+	    System.out.println();
+	    System.out.println("What do you want to do?");
+	    System.out.println("You can:");
+
+	    try {
+		for (int i=0;i<warriorMethods.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(warriorMethods[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    attackmethod = scanner.nextLine();
+	    if (attackmethod.toUpperCase().equals("BASH")) {
+		p1.bash(enemy);
+	    } else if (attackmethod.toUpperCase().equals("BLOCK")) {
+		p1.block(enemy);
+	    }
+	    System.out.println(enemy.getHealth());
+	    
+
+
+
+
+	} else if (character.equals("mage")) {
+	    mage p1 = new mage(newname);
+	    String parsing[] = { "Your adventure begins now!...", "...","... ", "...", "..." };
+	    String parsing2[] = { "...", "...", "...", "...", "....!!" };
+	    try {
+		for (int i=0;i<parsing.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(parsing[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+
+	    monster enemy = new monster();
+	    System.out.println();
+	    System.out.println("You are a "+character.toLowerCase()+" named "+newname.toUpperCase()+".");
+	    System.out.println("You walk through the forest, with a firm grip on your wand.");
+	    try { Thread.sleep(1000); } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    try {
+		for (int i=0;i<parsing.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(parsing2[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    String mageMethods[] = {"->FIRESPELL","->HEAL"};
+	    System.out.println("You encountered an "+enemy.getName().toUpperCase()+"!");
+	    System.out.println();
+	    System.out.println("What do you want to do?");
+	    System.out.println("You can:");
+
+	    try {
+		for (int i=0;i<mageMethods.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(mageMethods[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    attackmethod = scanner.nextLine();
+	    if (attackmethod.toUpperCase().equals("FIRESPELL")) {
+		p1.firespell(enemy);
+	    } else if (attackmethod.toUpperCase().equals("HEAL")) {
+		p1.heal(enemy);
+	    }
+	    System.out.println(enemy.getHealth());
+	    
+
+
+
+	} else if (character.equals("rogue")) {
+	    rogue p1 = new rogue(newname);
+	    String parsing[] = { "Your adventure begins now!...", "...","... ", "...", "..." };
+	    String parsing2[] = { "...", "...", "...", "...", "....!!" };
+	    try {
+		for (int i=0;i<parsing.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(parsing[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+
+	    monster enemy = new monster();
+	    System.out.println();
+	    System.out.println("You are a "+character.toLowerCase()+" named "+newname.toUpperCase()+".");
+	    System.out.println("You walk through the forest with your dagger up your sleeve.");
+	    try { Thread.sleep(1000); } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    try {
+		for (int i=0;i<parsing.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(parsing2[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    String rogueMethods[] = {"->SLASH","->DODGE"};
+	    System.out.println("You encountered an "+enemy.getName().toUpperCase()+"!");
+	    System.out.println();
+	    System.out.println("What do you want to do?");
+	    System.out.println("You can:");
+
+	    try {
+		for (int i=0;i<rogueMethods.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(rogueMethods[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+	    attackmethod = scanner.nextLine();
+	    if (attackmethod.toUpperCase().equals("SLASH")) {
+		p1.slash(enemy);
+	    } else if (attackmethod.toUpperCase().equals("DODGE")) {
+		p1.dodge(enemy);
+	    }
+	    System.out.println(enemy.getHealth());
+	    
+	} else if (character.equals("")) {
+	    warrior p1 = new warrior(newname);
+	    String parsing[] = { "Your adventure begins now!...", "...","... ", "...", "..." };
+	    String parsing2[] = { "...", "...", "...", "...", "....!!" };
+	    try {
+		for (int i=0;i<parsing.length;i++){
+		    Thread.sleep(500);
+		    System.out.println(parsing[i]);
+		}
+		Thread.sleep(1000);
+	    } catch(InterruptedException ex) {
+		Thread.currentThread().interrupt();
+	    }
+
+	    monster enemy = new monster();
+	    System.out.println();
+	    System.out.println("You are a "+character.toLowerCase()+" named "+newname.toUpperCase()+".");
 	    System.out.println("You walk through the forest with your weapon on your shoulder.");
 	    try { Thread.sleep(1000); } catch(InterruptedException ex) {
 		Thread.currentThread().interrupt();
@@ -82,14 +248,6 @@ public class Driver {
 	    }
 	    System.out.println(enemy.getHealth());
 	    
-	} else if (character.equals("mage")) {
-	    mage p1 = new mage(newname);
-	    
-	} else if (character.equals("rogue")) {
-	    rogue p1 = new rogue(newname);
-	   
-	} else if (character.equals("")) {
-	    warrior p1 = new warrior(newname);
 	}
 
 
