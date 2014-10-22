f3509d4edc1be9a1b31c97822301c739bf343354
Derrick Lui <derricklui1@gmail.com>
2014-10-21 17:53:31 -0700

    Added input methods

---
+++ Driver.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Driver {
+    public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	String character;
+	String newname;
+	System.out.println();
+	System.out.println("Welcome to Stuyablo!");
+	System.out.println();
+	System.out.println("What class do you want to be?");
+	System.out.println("-> Warrior");
+	System.out.println("-> Mage");
+	System.out.println("-> Rogue");
+	character = scanner.nextLine();
+	System.out.println("Please choose a name for yourself:");
+        newname = scanner.nextLine();
+	System.out.println("You are a "+character.toLowerCase()+" named "+newname+".");
+
+	    
+
+    }
+}
